cf505b10d0368d244910b07fbe54f389c8e366bc
max19931 <53226461+max19931@users.noreply.github.com>
2019-08-16 10:45:09 +0200

    Clean up

--- dist/spop.js
+++ dist/spop.js
@@ -50,42 +50,25 @@
 
 		container = $(this.getPosition('spop--', this.opt.position));
 
-		icon = (!this.opt.icon) ? '' : '<i class="spop-icon '+
-		            this.getStyle('spop-icon--', this.opt.style) +'"></i>';
+		icon=(!this.opt.icon)?'':'<i class="spop-icon '+this.getStyle('spop-icon--', this.opt.style) +'"></i>';
 
-		layout ='<div class="spop-close" data-spop="close" aria-label="Close">&times;</div>' +
-						icon +
-					'<div class="spop-body">' +
-						template +
-					'</div>';
+		layout ='<div class="spop-close" data-spop="close" aria-label="Close">&times;</div>'+icon +'<div class="spop-body">'+template+'</div>';
 
 		if (!container) {
 
 			this.popContainer = document.createElement('div');
 
-			this.popContainer.setAttribute('class', 'spop-container ' +
-				this.getPosition('spop--', this.opt.position));
-
+			this.popContainer.setAttribute('class', 'spop-container '+this.getPosition('spop--', this.opt.position));
 			this.popContainer.setAttribute('id', this.getPosition('spop--', this.opt.position));
-
 			document.body.appendChild(this.popContainer);
-
 			container = $(this.getPosition('spop--', this.opt.position));
 		}
 
 		this.pop = document.createElement('div');
-
 		this.pop.setAttribute('class', 'spop spop--out spop--in ' + this.getStyle('spop--', this.opt.style) );
-
-		if (this.opt.group && typeof this.opt.group === 'string') {
-			this.pop.setAttribute('id', 'spop--' + this.opt.group);
-		}
-
-
+		if (this.opt.group && typeof this.opt.group === 'string') {this.pop.setAttribute('id', 'spop--' + this.opt.group);}
 		this.pop.setAttribute('role', 'alert');
-
 		this.pop.innerHTML = layout;
-
 		container.appendChild(this.pop);
 	};
 
@@ -113,81 +96,54 @@
 	};
 
 	SmallPop.prototype.open = function() {
-
 		this.create(this.opt.template);
-
 		if (this.opt.onOpen) { this.opt.onOpen();}
-
 		this.close();
 	};
 
 	SmallPop.prototype.close = function () {
-
 		if (this.opt.autoclose && typeof this.opt.autoclose === 'number') {
-
 			this.autocloseTimer = setTimeout( this.remove.bind(this, this.pop), this.opt.autoclose);
 		}
-
 		this.pop.addEventListener('click', this.addListeners.bind(this) , false);
 	};
 
 	SmallPop.prototype.addListeners = function(event) {
-
 		close = event.target.getAttribute('data-spop');
-
 		if (close === 'close') {
-
 			if (this.autocloseTimer) { clearTimeout(this.autocloseTimer);}
-
 			this.remove(this.pop);
 		}
 	};
 
 	SmallPop.prototype.remove = function(elm) {
-
 		if (this.opt.onClose) { this.opt.onClose();}
-
 		removeClass(elm, 'spop--in');
-
 		setTimeout( function () {
-
 			if(document.body.contains(elm)) {
 				elm.parentNode.removeChild(elm);
 			}
-
 		}, animationTime);
 	};
-
-
 	// Helpers
-
-	function $(el, con) {
-		return typeof el === 'string'? (con || document).getElementById(el) : el || null;
-	}
-
+	function $(el, con) {return typeof el === 'string'? (con || document).getElementById(el) : el || null;}
 	function removeClass(el, className) {
 		if (el.classList) {
 			el.classList.remove(className);
 		}
 		else {
-			el.className = el.className.replace(new RegExp('(^|\\b)' +
-							className.split(' ').join('|') +
-							'(\\b|$)', 'gi'), ' ');
+			el.className=el.className.replace(new RegExp('(^|\\b)'+className.split(' ').join('|')+'(\\b|$)','gi'),' ');
 		}
 	}
-
 	function extend(obj, src) {
-
 		for (var key in src) {
 			if (src.hasOwnProperty(key)) obj[key] = src[key];
 		}
-
 		return obj;
 	}
 
 	window.spop = function (template, style) {
 		if ( !template || !window.addEventListener ) { return false;}
-
 		return new SmallPop(template, style);
 	};
 
